3d112f067447303731f94e14426d521b1dbf81e3
Lukas Hirt <lukashirt@mail.com>
2023-03-31 17:42:43 +0200

    chore: use https and enable docker host

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ module.exports = {
   configureWebpack: {
     resolve: {
       symlinks: false
+    },
+    devServer: {
+      allowedHosts: ['localhost', 'host.docker.internal'],
+      https: true
     }
   },
   css: {
